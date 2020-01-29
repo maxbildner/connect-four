7c76512211a4c690a5cf1ce978667f47c2b76cc9
mbildner <mbildner@icloud.com>
2020-01-29 22:13:54 +0800

    Updates

---
+++ precache-manifest.7ec032ec78dec55004dd786e1e33f9c4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4104d2d24f9545576076a92d7d61abda",
+    "url": "/connect-four/index.html"
+  },
+  {
+    "revision": "c4bfc257379eccc629cf",
+    "url": "/connect-four/static/css/main.e1bff7d7.chunk.css"
+  },
+  {
+    "revision": "1d172e7b9baaa9462cbe",
+    "url": "/connect-four/static/js/2.155e02f1.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/connect-four/static/js/2.155e02f1.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c4bfc257379eccc629cf",
+    "url": "/connect-four/static/js/main.73ab1b73.chunk.js"
+  },
+  {
+    "revision": "8387ea76592142a3f37d",
+    "url": "/connect-four/static/js/runtime-main.1abdecb1.js"
+  }
+]);
